851ceca5ec12604aace94ff70ec1337a7c1fa1c7
Bhushan <80047431+bhushan2611@users.noreply.github.com>
2024-02-01 21:18:59 +0530

    number pyramid program

---
+++ numPyramid.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout<<"enter n: ";
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        int count = 0;
+        for(int j=1;j<n+i;j++){
+            if(j<=n-i) cout<<"  ";
+            else if(j>n-i and j<=n) cout<<++count<<" ";
+            else cout<<--count<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+/*
+_ _ _ 1
+_ _ 1 2 1
+_ 1 2 3 2 1
+1 2 3 4 3 2 1
+
+
+*/
